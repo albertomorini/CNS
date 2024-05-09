60335afc37f74db9842b201b1f62c2e8b325b8a0
mhetacc <marco.bello.vi@gmail.com>
2024-05-09 18:15:11 +0200

    json to data frame update

--- src_analyzer/jsonToDataFrame.r
+++ src_analyzer/jsonToDataFrame.r
@@ -2,7 +2,7 @@ library(jsonlite)
 
 
 
-mockUpJson <- fromJSON("src_analyzer/mockUp.json")
+mockUpJson <- fromJSON("msc/mockUp.json")
 
 sample_data <- mockUpJson
 output_dataframe <- as.data.frame(sample_data)
