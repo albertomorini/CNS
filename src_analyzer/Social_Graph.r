ec645181102bf28ee976034eb9c9742542747192
mhetacc <marco.bello.vi@gmail.com>
2024-05-14 18:21:37 +0200

    san graph colored

--- src_analyzer/Social_Graph.r
+++ src_analyzer/Social_Graph.r
@@ -5,8 +5,11 @@ library(tibble)
 library(ggplot2)
 
 
-#json_data <- fromJSON(paste(readLines("data_downloaded/2024_2_1x30.json")))
-json_data <- fromJSON(paste(readLines("msc/influencersFollowersMorckUp.json")))
+
+#OSS: there are duplicated user for the same influencer (for different days)
+# therefore -> some follower nodes have degree > 1
+json_data <- fromJSON(paste(readLines("data_downloaded/2024_2_1x30.json")))
+#json_data <- fromJSON(paste(readLines("msc/influencersFollowersMorckUp.json")))
 
 
 
@@ -26,7 +29,7 @@ for (name in influencer_names){
 }
 
 # create SAN
-nodes <- data.frame(total$influencer, total$followers)
+nodes <- data.frame(total$influencer["huffpost"], total$followers)
 # View(nodes)
 
 library(igraph) # load here and then detach otherwise error with $ and vectors
@@ -36,6 +39,13 @@ my_san <- graph_from_data_frame(nodes, directed = FALSE)
 # add lables
 V(my_san)$label <- V(my_san)$name
 
-# TODO format graphical aspectsof plotted san
-plot(my_san) # successfully plotted social graph
+# TODO format graphical aspects of plotted san
+plot(
+  my_san,
+  main = "graph label",
+  vertex.label = ifelse(degree(my_san) == 4, V(my_san)$label, NA),
+  vertex.size = 1,
+  edge.width = 1.5,
+  vertex.color = c("cyan", "tomato")[1 + names(V(my_san)) %in% influencer_names],
+  ) 
 detach(package:igraph)
