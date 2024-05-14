89017de7ce344bf55f55cbefb8d1cb4d3711cda4
mhetacc <marco.bello.vi@gmail.com>
2024-05-14 11:54:57 +0200

    san graph minimum viable product

--- src_analyzer/Social_Graph.r
+++ src_analyzer/Social_Graph.r
@@ -3,7 +3,12 @@ library(dplyr)
 library(tidyverse)
 library(tibble)
 library(ggplot2)
-json_data <- fromJSON(paste(readLines("data_downloaded/2024_2_1x30.json")))
+
+
+#json_data <- fromJSON(paste(readLines("data_downloaded/2024_2_1x30.json")))
+json_data <- fromJSON(paste(readLines("msc/influencersFollowersMorckUp.json")))
+
+
 
 influencer_names <- names(json_data$stored)  
 dates <- names(json_data$stored[[influencer_names[1]]])  
@@ -12,15 +17,34 @@ InfXDate <- crossing(influencer_names,dates) ## Cartesian product between Influe
 
 total <- tibble()
 # TODO: optimize using the cartesian product
-for(nome in influencer_names){
+for(name in influencer_names){
   for(d in dates){
     dummy <- tibble(
-      influencer = nome,
+      influencer = name,
       day = d,
-      followers = as.vector(sapply(json_data$stored$huffpost[[day]]$data$user_followers, function(x) x$username)) # vector of usernames as strings ("user1", ..., "user n")
+      followers = sapply(json_data$stored[[influencer]][[day]]$data$user_followers, function(x) x$username) # vector of usernames as strings ("user1", ..., "user n")
     )
     total<- rbind(total,dummy)
 
     #print(length(as.vector(json_data$stored[[nome]][[d]]$data$user_followers[[1]]$username,mode='list'))) ##works
   }
 }
+
+#View(total)
+# total now is a table influencer X day X follower 
+
+
+# create SAN
+nodes <- data.frame(total$influencer, total$followers)
+View(nodes)
+
+library(igraph)
+my_san <- graph_from_data_frame(nodes, directed = TRUE)
+View(my_san)
+
+
+# add lables
+V(my_san)$label <- V(my_san)$name
+
+plot(my_san)
+detach(package:igraph)
