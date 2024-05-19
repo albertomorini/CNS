4f2cfa44f49bc0f92d18ad394e6f1942fd808164
albertomorini <99morini@gmail.com>
2024-05-19 17:35:10 +0200

    huge performance optimization

--- src_analyzer/Analysis_OfficialData.r
+++ src_analyzer/Analysis_OfficialData.r
@@ -3,70 +3,42 @@ library(dplyr)
 library(tidyverse)
 library(tibble)
 library(ggplot2)
-json_data <- fromJSON(paste(readLines("data_downloaded/2024_2_1x30.json")))
+json_data <- fromJSON(paste(readLines("data_downloaded/2024_5_17x40.json")))
 
 influencer_names <- names(json_data$stored)  
 dates <- names(json_data$stored[[influencer_names[1]]])   
 
-InfXDate <- crossing(influencer_names,dates) ## Cartesian product between InfluencersName and date
+InfXDate <- as_tibble(crossing(influencer_names,dates)) ## Cartesian product between InfluencersName and date
 
-
-
-
-
-
-total <- tibble()
-# TODO: optimize using the cartesian product
-for(nome in influencer_names){
-  for(d in dates){
-    dummy <- tibble(
-      influencer = nome,
-      day = d,
-      followers = as.vector(json_data$stored[[nome]][[d]]$data$user_followers,mode='list') ##TODO: keep just the username
-    )
-    total<- rbind(total,dummy)
-    #print(length(as.vector(json_data$stored[[nome]][[d]]$data$user_followers[[1]]$username,mode='list'))) ##works
-  }
-}
-
-
-ff = function(influencer,day,followers){
-    tmp <- total %>%
-    as_tibble %>%
-    rowwise() %>%
-    mutate (tt = length(followers$username)) %>%
-    sort_by(day) %>%
-    select(influencer,day,tt)
-    # TODO: remove duplicated followers 
-    # select(influencer, day,length(tt))
-  
-
-    # 
-    ggplot(tmp, aes(x = day, y = tt, group=influencer, colour=influencer))+
-      geom_line(size=1.2)+
-      scale_color_manual(values=c("black", "#d8ce15"))+
-      labs(x = "Days",
-           y = "Amount of followers",
-           colour = "Influencers")+
-      theme_minimal()+
-      theme(
-        axis.text.x = element_text(angle=70)
-      )+
-      ggtitle("Number of followers", "TODO: inserire il mese")
-   
-}
-
-ff(total$influencer, total$day, total$followers)
-
-  # select(influencer,day, )
-
-
-
-
-
-marco<-total %>%
+## for cartesian correlation (Influencer : day) we put the list of the nickname of that day
+total <- InfXDate %>%
   as_tibble %>%
   rowwise() %>%
-  summarise(u= influencer) %>%
-  mutate (a = list(followers$username)) %>%
-  select(u,a)
+  mutate (followers = list(json_data$stored[[influencer_names]][[dates]]$data$user_followers[[1]]$username)) %>%
+  select (influencer = influencer_names, day = dates,followers)
+
+
+
+# # 
+# ggplot(total, aes(x = day, y = length(followers), group=influencer, colour=influencer))+
+# geom_line(size=1.2)+
+# scale_color_manual(values=c("black", "#d8ce15","red","pink","blue"))+
+# labs(x = "Days",
+#      y = "Amount of followers",
+#      colour = "Influencers")+
+# theme_minimal()+
+# theme(
+#   axis.text.x = element_text(angle=70)
+# )+
+# ggtitle("Number of followers", "TODO: inserire il mese")
+
+#   
+
+
+# 
+# marco<-total %>%
+#   as_tibble %>%
+#   rowwise() %>%
+#   summarise(u= influencer) %>%
+#   mutate (a = list(followers$username)) %>%
+#   select(u,a)
